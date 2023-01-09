57b454a9b8d0e83f29d259dd572fd19a38d22dba
SAKTHIVEL.K <114995545+sakthivel612@users.noreply.github.com>
2023-01-09 02:08:15 -0800

    add the resume
    add the resume

---
+++ indexresum.js
@@ -0,0 +1,10 @@
+var Resume = [{
+    "Name" : "Sakthivel.k",
+    "Age" : "22",
+    "Phone_number" : "+91 ---",
+    "Gender" : "Male",
+    "Email" : "---@gmail.com",
+    "Qualifications" : "Msc(Computer science)",
+    "Languages" : "Tamil,English",
+    "Address" : "------"
+}]
